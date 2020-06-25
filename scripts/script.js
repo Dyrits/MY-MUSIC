d716f4aa9c85d675940c42eb59e1e90007729346
dyrits <dyrits@outlook.fr>
2020-06-25 16:50:26 +0200

    New script.

--- scripts/script.js
+++ scripts/script.js
@@ -1,79 +1,84 @@
 const audioPlayer = document.querySelector("#audio-player");
 const volume = document.querySelector("#volume");
 const playPause = document.querySelector("#play-pause");
-const trackPosition = document.querySelector("#track-position");
+const trackPosition = document.querySelector("#track-position")
 let currentTrack = media[0];
 
 volume.value = audioPlayer.volume;
-loadTrack();
+loadTrack()
 
-setInterval(() => {
+volume.oninput = () => { audioPlayer.volume = volume.value; }
+
+setInterval( () => {
   trackPosition.value = audioPlayer.currentTime;
   audioPlayer.currentTime === audioPlayer.duration && nextTrack();
 }, 50)
 
 audioPlayer.onloadeddata = () => {
-  if (audioPlayer.readyState > 1) { trackPosition.max = Math.floor(audioPlayer.duration); }
+  if (audioPlayer.readyState > 1) {
+    trackPosition.max = Math.floor(audioPlayer.duration);
+  }
 }
+
+document.querySelector("#volume-down").addEventListener("click", () => {
+  audioPlayer.volume -= audioPlayer.volume > 0 && 0.05;
+  volume.value = audioPlayer.volume;
+})
+
+document.querySelector("#volume-up").addEventListener("click", () => {
+  audioPlayer.volume += audioPlayer.volume < 1 && 0.05;
+  volume.value = audioPlayer.volume;
+})
 
 trackPosition.addEventListener("input", () => {
   audioPlayer.currentTime = trackPosition.value;
 })
 
+playPause.addEventListener("click", () => {
+  audioPlayer.paused ? play() : pause();
+})
+
+document.querySelector("#previous").onclick = previousTrack;
+document.querySelector("#next").onclick = nextTrack;
+
 document.querySelectorAll(".card").forEach((card, index) => {
   card.addEventListener("click", () => {
     currentTrack = media[index];
     loadTrack();
-  });
+  })
   card.addEventListener("dblclick", () => {
     if (audioPlayer.paused) {
       currentTrack = media[index];
       loadTrack();
       play();
     }
-  });
+  })
 })
 
-document.querySelector("#previous").onclick = previousTrack;
-document.querySelector("#next").onclick = nextTrack;
-
-// Volume
-volume.oninput = () => { audioPlayer.volume = volume.value; }
-
-document.querySelector("#volume-down").addEventListener("click", () => {
-    audioPlayer.volume -= audioPlayer.volume > 0 && 0.05;
-    volume.value = audioPlayer.volume;
-})
-
-document.querySelector("#volume-up").addEventListener("click", () => {
-    audioPlayer.volume += audioPlayer.volume < 1 && 0.05;
-    volume.value = audioPlayer.volume;
-})
-
-// Play/Pause
-playPause.addEventListener("click", () => {
-  audioPlayer.paused ? play() : pause();
-})
 
 function play() {
-  playPause.firstChild.className = "fas fa-pause";
   audioPlayer.play();
+  playPause.firstChild.className = "fas fa-pause";
 }
 
 function pause() {
-  playPause.firstChild.className = "fas fa-play";
   audioPlayer.pause();
+  playPause.firstChild.className = "fas fa-play";
 }
 
 function previousTrack() {
-  if (audioPlayer.currentTime > audioPlayer.duration / 5) { loadTrack(); }
-  else { currentTrack = currentTrack === media[0] ? media[media.length - 1] : media[media.indexOf(currentTrack) - 1]; }
-  loadTrack()
+  if (audioPlayer.currentTime > audioPlayer.duration / 5) {
+    loadTrack()
+  }
+  else {
+    currentTrack = currentTrack === media[0] ? media[media.length - 1] : media[media.indexOf(currentTrack) - 1];
+    loadTrack();
+  }
 }
 
 function nextTrack() {
   currentTrack = currentTrack === media[media.length - 1] ? media[0] : media[media.indexOf(currentTrack) + 1];
-  loadTrack()
+  loadTrack();
 }
 
 function loadTrack() {
@@ -82,7 +87,7 @@ function loadTrack() {
   document.querySelector("#source-WAV").src = `./media/${currentTrack.file}.wav`
   updateVisual();
   let paused = audioPlayer.paused;
-  audioPlayer.load()
+  audioPlayer.load();
   !paused && audioPlayer.play();
 }
 
@@ -93,10 +98,10 @@ function updateVisual() {
     if (image.src.includes(currentTrack.photo)) {
       image.style.padding = "5px";
       image.style.borderRadius = "25px 0 25px 0";
-      image.style.transform = "translateY(-4px) scale(1.075) rotate(1.5deg)"
+      image.style.transform = "translateY(-4px) scale(1.075) rotate(1.5deg)";
     } else {
+      image.style.padding = "";
       image.style.borderRadius = "";
-      image.style.border = "";
       image.style.transform = "";
     }
   })
